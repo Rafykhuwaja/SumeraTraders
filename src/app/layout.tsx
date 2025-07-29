07cc2d015d48b84733a0610b6b46bc29e44c8c5f
Muhammad Uzair <uzairshaikh346@gmail.com>
2025-07-29 17:29:00 +0500

    set code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black`}>
-      <div className="min-h-screen bg-gradient-to-br from-[#e87b51]/50 via-black/15 to-[#a7d8de]/30 text-white">
-
+      <body className={`${inter.className} min-h-screen bg-gradient-to-br from-[#e87b51]/50 via-black/15 to-[#a7d8de]/30 text-white`}>
         <div className="min-h-screen flex flex-col">
           <Navbar />
           <main className="flex-grow">
@@ -31,7 +29,6 @@ export default function RootLayout({
           </main>
           <Footer />
         </div>
-      </div>
       </body>
     </html>
   );
